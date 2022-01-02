b11a692ed21e88327058f5294230bd1f59015156
leonardogagliano <leonardogag3@gmail.com>
2022-01-02 13:54:17 -0300

    project commit

--- lessons and studies/Kenzie Academy Brasil/html, CSS and Javascript/aula4 - Avançando em Javascript/script.js
+++ lessons and studies/Kenzie Academy Brasil/html, CSS and Javascript/aula4 - Avançando em Javascript/script.js
@@ -9,6 +9,16 @@ console.log(soma(1,2));
 let contar = 0;
 
 while(contar < 15){
-	console.log(contar);
+	console.log(contar + " oi");
 	contar++;
+}
+
+do{
+	console.log("uma vez");
+}while(1==0) //esta condição é falsa, mas o código será executado uma vez antes de ela ser verificada.
+
+
+
+for(let contar = 0; contar <20; contar+=1){
+		console.log(contar);
 }
